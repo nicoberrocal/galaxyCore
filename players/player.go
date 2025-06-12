0443baf0c0a10a77f3809f595fdcf3ef6d7c39d5
Nicolas <nicolasberrocal@gmail.com>
2025-06-12 15:51:56 -0300

    FIX: Added fields to Player

--- players/player.go
+++ players/player.go
@@ -1,10 +1,17 @@
 package players
 
-import "go.mongodb.org/mongo-driver/v2/bson"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/v2/bson"
+)
 
 type Player struct {
-	ID       bson.ObjectID `bson:"_id,omitempty"`
-	Username string        `bson:"username"`
-	Email    string        `bson:"email"`
-	Password string        `bson:"password"`
+	ID         bson.ObjectID `bson:"_id,omitempty"`
+	Username   string        `bson:"username"`
+	Email      string        `bson:"email"`
+	Password   string        `bson:"password"`
+	IsVerified bool          `bson:"verified"`
+	IsPremium  bool          `bson:"is_premium"`
+	CreatedAt  time.Time     `bson:"created_at"`
 }
