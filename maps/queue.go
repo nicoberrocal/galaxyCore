8534b3d15ebda7a90c4410be547e6cd8ac72b0e6
Nicolas <nicolasberrocal@gmail.com>
2025-07-29 15:10:33 -0300

    FIX: Add PredictedEvents tos Queue

--- maps/queue.go
+++ maps/queue.go
@@ -8,22 +8,23 @@ import (
 
 // PlayerAction represents an action initiated by a player
 type Queue struct {
-	ID          bson.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
-	PlayerID    bson.ObjectID `bson:"playerId" json:"playerId"`
-	MapID       bson.ObjectID `bson:"mapId" json:"mapId"`
-	Type        string        `bson:"type" json:"type"`                             // ship_attack, ship_construction, building_construction, ship_ability
-	TargetID    bson.ObjectID `bson:"targetId,omitempty" json:"targetId,omitempty"` // Target of the action (e.g., ship, building)
-	SourceID    bson.ObjectID `bson:"sourceId,omitempty" json:"sourceId,omitempty"` // Source of the action (e.g., ship, building)
-	StartX      float64       `bson:"startX,omitempty" json:"startX,omitempty"`     // Starting coordinates
-	StartY      float64       `bson:"startY,omitempty" json:"startY"`
-	TargetX     float64       `bson:"targetX,omitempty" json:"targetX"` // Target coordinates
-	TargetY     float64       `bson:"targetY,omitempty" json:"targetY"`
-	StartTime   time.Time     `bson:"startTime" json:"startTime"`
-	EndTime     time.Time     `bson:"endTime,omitempty" json:"endTime"`
-	CreatedAt   time.Time     `bson:"createdAt" json:"createdAt"`                 // When the action was created
-	ProcessedAt time.Time     `bson:"processedAt,omitempty" json:"processedAt"`   // When the action was processed
-	Version     int64         `bson:"version" json:"version"`                     // For optimistic locking
-	Payload     bson.D        `bson:"payload,omitempty" json:"payload,omitempty"` // Additional action-specific data
+	ID              bson.ObjectID    `bson:"_id,omitempty" json:"_id,omitempty"`
+	PlayerID        bson.ObjectID    `bson:"playerId" json:"playerId"`
+	MapID           bson.ObjectID    `bson:"mapId" json:"mapId"`
+	Type            string           `bson:"type" json:"type"`                             // ship_attack, ship_construction, building_construction, ship_ability
+	TargetID        bson.ObjectID    `bson:"targetId,omitempty" json:"targetId,omitempty"` // Target of the action (e.g., ship, building)
+	SourceID        bson.ObjectID    `bson:"sourceId,omitempty" json:"sourceId,omitempty"` // Source of the action (e.g., ship, building)
+	StartX          float64          `bson:"startX,omitempty" json:"startX,omitempty"`     // Starting coordinates
+	StartY          float64          `bson:"startY,omitempty" json:"startY"`
+	TargetX         float64          `bson:"targetX,omitempty" json:"targetX"` // Target coordinates
+	TargetY         float64          `bson:"targetY,omitempty" json:"targetY"`
+	StartTime       time.Time        `bson:"startTime" json:"startTime"`
+	EndTime         time.Time        `bson:"endTime,omitempty" json:"endTime"`
+	CreatedAt       time.Time        `bson:"createdAt" json:"createdAt"`               // When the action was created
+	ProcessedAt     time.Time        `bson:"processedAt,omitempty" json:"processedAt"` // When the action was processed
+	PredictedEvents []PredictedEvent `bson:"predictedEvents,omitempty" json:"predictedEvents,omitempty"`
+	Version         int64            `bson:"version" json:"version"`                     // For optimistic locking
+	Payload         bson.D           `bson:"payload,omitempty" json:"payload,omitempty"` // Additional action-specific data
 }
 
 type ResponseQueue struct {
