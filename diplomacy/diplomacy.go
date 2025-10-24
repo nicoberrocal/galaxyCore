7479bee637e3f0a60427b23a25a57f7ce0de1a1b
Nicolas <nicolasberrocal@gmail.com>
2025-10-23 23:33:35 -0300

    FEATURE: ONLY DEFINITION DIPLOMACY

--- diplomacy/diplomacy.go
+++ diplomacy/diplomacy.go
@@ -6,12 +6,21 @@ import (
 	"go.mongodb.org/mongo-driver/v2/bson"
 )
 
+type Relation string
+
+const (
+	RelationUnknown   Relation = "unknown"
+	RelationEnemy     Relation = "enemy"
+	RelationAlly      Relation = "ally"
+	RelationCeasefire Relation = "ceasefire"
+)
+
 type RelationDoc struct {
 	ID        bson.ObjectID `bson:"_id,omitempty"`
 	MapID     bson.ObjectID `bson:"mapId"`
 	Player1   bson.ObjectID `bson:"player1"`
 	Player2   bson.ObjectID `bson:"player2"`
-	Relation  int32         `bson:"relation"`
+	Relation  Relation      `bson:"relation"`
 	Until     time.Time     `bson:"until,omitempty"`
 	CreatedAt time.Time     `bson:"createdAt"`
 	UpdatedAt time.Time     `bson:"updatedAt"`
